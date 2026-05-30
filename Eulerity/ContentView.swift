00a5b90d2b0055b4a053ffa221a33047b49402a3
Rojo jacob <rojo@wacmob.com>
2026-05-30 10:03:45 +0530

    Initial commit from Specify template

---
+++ Eulerity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Eulerity
+//
+//  Created by Rojo on 30/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Eulerity/EulerityApp.swift
@@ -0,0 +1,17 @@
+//
+//  EulerityApp.swift
+//  Eulerity
+//
+//  Created by Rojo on 30/05/26.
+//
+
+import SwiftUI
+
+@main
+struct EulerityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
